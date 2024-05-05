85c37de661a4d0e4cc4908766c682d069263a809
Wonder947 <123397090+Wonder947@users.noreply.github.com>
2024-05-05 18:30:56 -0400

    first commit

--- README.md
+++ README.md
@@ -1,36 +1,9 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+## Objective
+This assignment involves the environmental data of a small-scale food processing plant.
+Your task is to build a full-stack app that imports data from the above Excel sheets and
+generates charts in the front end. Essentially, create a dashboard that showcases the current
+environmental metrics of the plant.
 
-## Getting Started
+## Instruction
+clone the repository, download Node.js 18.17 or later, run 'npm run dev' to see the result page.
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import styles from './page.module.css'
+import { useEffect, useState } from "react"
+import Papa from 'papaparse'
+import { Bar } from 'react-chartjs-2'
+import Chart from 'chart.js/auto'
+
+export default function Home(){
+  const [data, setData] = useState(null)
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const response = await fetch('/data/test_data.csv')
+      const csvData = await response.text()
+      // console.log('csv data:', csvData)
+      const parsedData = Papa.parse(csvData).data.slice(0, -1)
+      // console.log('parsed data:', parsedData)
+      const myData = {
+        dates: parsedData[0].slice(1),
+        metrics: parsedData.slice(1).map(arr=>arr[0]),
+        data: parsedData.slice(1).map(arr=>arr.slice(1))
+      }
+      console.log('my data:', myData)
+      const data = {
+        labels: myData.dates,
+        datasets: myData.metrics.map((m, i) => ({
+          label: m,
+          data: myData.data[i],
+          borderWidth: 1
+        }))
+      }
+      setData(data)
+    }
+    fetchData()
+  }, [])
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <h3>Current Environmental Metrics of the Plant</h3>
+      <div className={styles.dashboardContainer}>
+        {data && <Bar data={data}/>}
       </div>
     </main>
-  );
+  )
 }
